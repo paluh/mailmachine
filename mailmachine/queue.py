e62be6cb1ab4919f818c41d576b1bc444e4e76ab
Tomasz Rybarczyk <paluho@gmail.com>
2020-03-06 10:35:23 +0100

    Preserve backward compatiblity regarding alternatives encoding

--- mailmachine/queue.py
+++ mailmachine/queue.py
@@ -21,9 +21,7 @@ class MailQueue(object):
 
         attachments = map(lambda a: ({'file_name': a[0], 'content': base64.b64encode(a[1]), 'mime': a[2]}),
                           attachments or [])
-
         alternatives = map(lambda a: (base64.b64encode(a[0]), a[1]), alternatives or [])
-
         self._queue.put({'subject': subject, 'body': body, 'from_email': from_email,
                          'recipients': recipients, 'alternatives': alternatives,
                          'attachments': attachments})
@@ -31,15 +29,44 @@ class MailQueue(object):
     def get(self, block=False, timeout=None):
         message = self._queue.get(block, timeout)
 
-        # Don't ask me what is going non here
+        # In general we want(!?) (I'm not sure about that)
+        # just accept base64 encoded
+        # anything and decode it to unicode if it is a text
+        # according to a given charset.
+        #
+        # TODO: Check character set here!
+        #
+        # Don't ask me why we ended up here
         # but I'm treating `text/*` mime as utf-8
         # strings so they are send as nice plain text
         # alternative...
+        #
         def decode_alternative(a):
-            if a[1].split('/')[0] == 'text':
-                return (base64.b64decode(a[0]).decode('utf-8'), a[1])
+            payload = a[0]
+            mime = a[1]
+            if mime.split('/')[0] == 'text':
+                try:
+                    buf = base64.b64decode(payload)
+                    try:
+                        # base64 encoded utf-8 text
+                        return(buf.decode('utf-8'), mime)
+                    except:
+                        # base64 encoded some text
+                        # we are returning buf
+                        # because email_message is able
+                        # to cope with that...
+                        return (buf, mime)
+                except:
+                    # XXX: Backward compatibility
+                    # non base64 encoded string
+                    if isinstance(payload, unicode):
+                        return (payload, mime)
+
             else:
-                return (base64.b64decode(a[0]), a[1])
+                # In case of anything else we should just pass buffer
+                # to the email_message. It can handle this by..
+                # encoding it back to base64 :-P
+                return (base64.b64decode(a[0]), mime)
 
         message['alternatives'] = map(decode_alternative, message['alternatives'])
 
